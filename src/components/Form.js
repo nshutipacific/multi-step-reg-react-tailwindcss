a17466fb14dccdf50dfe7c003b45ea876c43eb70
Pacifique Nshuti <pacific.nsuti@zatechub.com>
2022-10-04 18:52:49 +0200

    Bug Fixing and Finetuning

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,18 +8,46 @@ function Form() {
     const FormTitles = ["Account Information", "Personal Information", "Other Information"];
 
     const StepToDisplay = ()=> {
-        if(page == 0){
+        if(page === 0){
             return <SignUpInfo/>
-        } else if ( page == 1){
+        } else if ( page === 1){
             return <PersonalInformation/>
         }else{
             return <OtherInformation/>
         }
     }
+
+    const getProgressBarClass = ()=> {
+        if(page === 0){
+            return "bg-gray-900 w-full h-2 p-2 rounded w-1/3"
+        } else if ( page === 1){
+            return "bg-gray-900 w-full h-2 p-2 rounded w-2/3"
+        }else{
+            return "bg-gray-900 w-full h-2 p-2 rounded w-3/3"
+        }
+    }
+
+    const [formData, setFormData] = useState({
+        userName : '',
+        email: '',
+        passowrd: '',
+
+        firstName: '',
+        lastName: '',
+        phoneNumber: 0,
+        idNumber: 0,
+
+        nationality: '',
+        city: '',
+        address: '',
+        zip: 0,
+    })
     return (
-        <div className="form">
-            <div className="progressbar"></div>
-            <div className="form-container">
+        <div className="form p-2">
+            <div className="progressbar bg-gray-300 rounded">
+                <div className={getProgressBarClass()} ></div>
+            </div>
+            <div className="form-container mt-2">
                 <div className="header py-4 font-black bg-gray-200">
                     <h1>{FormTitles[page]}</h1>
                 </div>
@@ -29,13 +57,13 @@ function Form() {
                 <div className="footer mt-4 mx-4 flex justify-between mt-4">
                     <button
                     className="bg-blue-700 py-1 px-4 text-white rounded"
-                    disabled={page == 0}
+                    disabled={page === 0}
                     onClick={()=> {
                         setPage((currPage) => currPage - 1);
                     }}>Prev</button>
                     <button
                     className="bg-blue-700 py-1 px-4 text-white rounded"
-                    disabled={page == FormTitles.length -1}
+                    disabled={page === FormTitles.length -1}
                     onClick={()=> {
                         setPage((currPage) => currPage + 1);
                     }}>Next</button>
--- src/components/SignUpInfo.js
+++ src/components/SignUpInfo.js
@@ -19,7 +19,7 @@ function SignUpInfo() {
 
             </div>
             <div className="flex justify-between mt-2 gap-4">
-                <div className="font-bold flex place-items-center">Password to Confirm</div>
+                <div className="font-bold flex place-items-center">Password 2</div>
                 <div><input className="bg-blue-100 rounded text-blue-700 shadow px-3 py-1 focus:outline-blue-700" type="text" name="username"/></div>
 
             </div>
